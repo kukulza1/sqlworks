bb8fc74b2e3339a17c5c7f46ecddbc0d671f0c98
kukulza1 <wlskrkek12345@naver.com>
2023-10-11 18:21:11 +0900

    10/11

---
+++ mydb/USERS.sql
@@ -0,0 +1,15 @@
+--자바프로젝트와 연동할 user테이블 생성
+CREATE TABLE users(
+   userid   VARCHAR2(20) PRIMARY KEY,
+   username VARCHAR2(30) NOT NULL,
+   userpw VARCHAR2(20) NOT NULL,
+   userage NUMBER(3) NOT NULL,
+   useremail VARCHAR2(50) NOT NULL
+);
+--회원추가
+INSERT INTO USERS VALUES('TODAY','고오늘','12345',25,'today@korea.kr');
+
+select*from users where userid = 'TODAY';
+
+commit;
+drop table users;
